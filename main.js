9c7cafe245c596cddd714fae799797cd1ba47388
Rinatelo <rinat.abzalutdinov@gmail.com>
2024-03-28 15:09:20 +0300

    fix: login verification

--- main.js
+++ main.js
@@ -2,7 +2,20 @@ import Todo from "./Todo.js";
 
 let user = localStorage.getItem("user");
 if (!user) {
-  user = window.prompt("Введите ваше имя:");
+  let userInput = window.prompt("Введите ваше имя:");
+  const handler = {
+    set: function (obj, prop, value) {
+      if (/\d|[.,\\/]/.test(value) || value.trim() === "") {
+        throw new Error("Недопустимое значение");
+      } else {
+        obj[prop] = value;
+        return true;
+      }
+    },
+  };
+  let userObj = new Proxy({}, handler);
+  userObj.name = userInput;
+  user = userObj.name;
   localStorage.setItem("user", user);
 }
 
